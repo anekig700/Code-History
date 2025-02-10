ded98929a86d508cd2c79dcb5e3260ce8e2d3feb
Ekaterina <anekig@icloud.com>
2025-02-10 20:11:14 +0000

    init

---
+++ Code History/Code_HistoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Code_HistoryApp.swift
+//  Code History
+//
+//  Created by Kotya on 10/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct Code_HistoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
