b8fc78500748478ab4ab390eeb4ac59ccff68b9f
Ekaterina <anekig@icloud.com>
2025-02-11 15:01:04 +0000

    create Question model and introduce ChoiceTextView

---
+++ Code History/Question.swift
@@ -0,0 +1,15 @@
+//
+//  Question.swift
+//  Code History
+//
+//  Created by Kotya on 11/02/2025.
+//
+
+import Foundation
+
+
+struct Question {
+    let questionText: String
+    let possibleAnswers: [String]
+    let correctAnswerIndex: Int
+}
